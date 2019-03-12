fc047417b29942b7b401807d9e32541266dd49c1
Shan Shen <shenshanpku@gmail.com>
2019-03-11 20:09:16 -0700

    Edit the datajoint basics.

---
+++ notebooks/insert_students.py
@@ -0,0 +1,37 @@
+import faker
+import random
+import itertools
+
+def make_students():
+    fake = faker.Faker()
+    fake_name = {'F': fake.name_female, 'M': fake.name_male}
+    while True:  # ignore invalid values
+        try:
+            sex = random.choice(('F', 'M'))
+            first_name, last_name = fake_name[sex]().split(' ')[:2]
+            street_address, city = fake.address().split('\n')
+            city, state = city.split(', ')
+            state, zipcode = state.split(' ')            
+        except ValueError:
+            continue
+        else:
+            yield dict(
+                first_name=first_name,
+                last_name=last_name,
+                sex=sex,
+                home_address=street_address,
+                home_city=city,
+                home_state=state,
+                home_zip=zipcode,
+                date_of_birth=str(
+                    fake.date_time_between(start_date="-35y", end_date="-15y").date()),
+                home_phone = fake.phone_number()[:20])
+            
+
+students = make_students()
+student_id = itertools.count()
+
+def insert(Student, number_of_students):
+    
+    for _, student in zip(range(number_of_students), students):
+        Student.insert1(dict(student, student_id=next(student_id)))
